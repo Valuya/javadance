dc5251a89b5b2f599567d34ebb7a8822e2f7b0d8
Fritz Elfert <fritz@fritz-elfert.de>
2015-08-19 09:33:20 +0200

    Ported OpenFileIT to jcifs

--- alfresco-jlan/src/test/java/org/alfresco/jlan/test/integration/OpenFileIT.java
+++ alfresco-jlan/src/test/java/org/alfresco/jlan/test/integration/OpenFileIT.java
@@ -24,23 +24,16 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import org.alfresco.jlan.client.CIFSDiskSession;
-import org.alfresco.jlan.client.CIFSFile;
-import org.alfresco.jlan.client.DiskSession;
-import org.alfresco.jlan.client.SMBFile;
-import org.alfresco.jlan.server.filesys.AccessMode;
-import org.alfresco.jlan.server.filesys.FileAction;
-import org.alfresco.jlan.server.filesys.FileAttribute;
-import org.alfresco.jlan.smb.SMBException;
-import org.alfresco.jlan.smb.SMBStatus;
-import org.alfresco.jlan.smb.SharingMode;
+import jcifs.smb.SmbFile;
+import jcifs.smb.SmbFileOutputStream;
+import jcifs.smb.SmbException;
 
 /**
  * Open File Test Class
  *
  * @author gkspencer
  */
-public class OpenFileIT extends ParameterizedIntegrationtest {
+public class OpenFileIT extends ParameterizedJcifsTest {
 
     /**
      * Default constructor
@@ -49,35 +42,25 @@ public class OpenFileIT extends ParameterizedIntegrationtest {
         super("OpenFileIT");
     }
 
-    private void doTest(int iteration) throws Exception {
-        DiskSession s = getSession();
-        assertTrue(s instanceof CIFSDiskSession, "Not an NT dialect CIFS session");
-        String testFileName = getPerTestFileName(iteration);
-        if (s.FileExists(testFileName)) {
+    private void doTest(final int iteration) throws Exception {
+        final String testFileName = getPerTestFileName(iteration);
+        final SmbFile sf = new SmbFile(getRoot(), testFileName);
+        if (sf.exists()) {
             LOGGER.info("File {} exists", testFileName);
         } else {
-            SMBFile testFile = s.CreateFile(testFileName);
-            if (testFile != null) {
-                testFile.Close();
-            }
-            assertTrue(s.FileExists(testFileName));
+            sf.createNewFile();
+            assertTrue(sf.exists(), "File exists after create");
         }
-        CIFSDiskSession cifsSess = (CIFSDiskSession)s;
-        CIFSFile openFile = null;
-        try {
-            // Open existing file for exclusive access, else fail
-            openFile = cifsSess.NTCreate("\\" + testFileName, AccessMode.NTReadWrite, FileAttribute.NTNormal,
-                    SharingMode.NOSHARING, FileAction.NTOpen, 0, 0);
-            if (null != openFile) {
+        // Open existing file for exclusive access, else fail
+        try (final SmbFileOutputStream os = new SmbFileOutputStream(sf.getCanonicalPath(), SmbFile.FILE_NO_SHARE)) {
+            if (null != os) {
                 // Hold the file open for a short while, other threads should fail to open the file
                 testSleep(2000);
-                // Close the test file
-                openFile.Close();
+                // Close the test file done by autoclose
             }
-        } catch (SMBException ex) {
+        } catch (SmbException ex) {
             // Check for an access denied error code
-            assertTrue(ex.getErrorClass() == SMBStatus.NTErr && ex.getErrorCode() == SMBStatus.NTAccessDenied,
-                    "Open failed with wrong error");
+            assertTrue(ex.getNtStatus() == SmbException.NT_STATUS_ACCESS_DENIED, "Open failed with wrong error");
             LOGGER.info("Open of {} failed with access denied error (expected)", testFileName);
         }
     }
